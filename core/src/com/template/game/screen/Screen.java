97837e2be71b089f83c6732b0c06a7c772e6f1e0
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2020-04-07 01:10:01 +0300

    added some todo notes

--- core/src/com/template/game/screen/Screen.java
+++ core/src/com/template/game/screen/Screen.java
@@ -69,8 +69,10 @@ public class Screen extends ScreenAdapter implements ScreenInterface {
         stage.draw();
 
         // TODO: refactor method into two threads - renderer and updater (which will be optional)
+        // TODO: create render queue, add shape renderer on top of the sprite batch
         // TODO: split rendering and updating responsibilities into two classes
         // TODO: register preferences, arguments, create ui template parsing from files
+        // TODO: maybe refactor this class just a little bit
     }
 
     @Override
