a87003a1716837d2334c4565722d83858b794ba3
Giedrius Kristinaitis <36892255+Giedrius-Kristinaitis@users.noreply.github.com>
2023-08-12 13:46:10 +0300

    Add files via upload

---
+++ desktop/src/com/gasis/undefined_game/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.gasis.undefined_game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.gasis.template.ApplicationBootstrapper;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("undefined");
+		new Lwjgl3Application(new ApplicationBootstrapper(), config);
+	}
+}
